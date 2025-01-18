fe1a44c83b0de6c4ad57b7a40a6eeb3cd887feab
Vraj <virajhiwale05@gmail.com>
2025-01-18 15:10:49 +0530

    First commit

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+# url dispatching
+
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.index, name='myapp'),
+    path("about/", views.about, name='about'),
+    path("services/", views.services, name='services'),
+    path("contact/", views.contact, name='contact'),
+]
---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+     return render(request, 'index.html')
+    # return HttpResponse("title is home page")
+
+def about(request):
+    return HttpResponse("title is about page")
+
+def services(request):
+    return HttpResponse("title is services page")
+
+def contact(request):
+    return HttpResponse("title is contact page")
